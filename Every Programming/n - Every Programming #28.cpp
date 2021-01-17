ea6d962b527747b5c30fdd6791aed376d6e3189b
gntodtndls156 <gntodtndls156@gmail.com>
2021-01-17 18:50:32 +0900

    add

---
+++ Every Programming/n - Every Programming #28.cpp
@@ -0,0 +1,12 @@
+/* 
+정수 배열이 주어지면, 배열 안의 모든 정수의 최대 공약수(GCD)를 구하시오.
+시간 복잡도 제한 O(n)
+
+Given an integer array, find the greatest common denominator of all elements.
+
+Intput: [3, 2, 1]
+Output: 1
+
+Input: [2, 4, 6, 8]
+Output: 2
+ */
